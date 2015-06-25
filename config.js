ca0f23d624a99a33868f859cba36b7bca2a19de5
Frederico Moreira <fredmacc@gmail.com>
2015-06-25 02:52:03 -0300

    Tests

--- config.js
+++ config.js
@@ -1,29 +1,39 @@
 exports.config = {
 
-  chromeDriver: './node_modules/protractor/selenium/chromedriver',
-  // Do not start a Selenium Standalone sever - only run this using chrome.
-  directConnect: false,
+
   specs: [
     'tests/ola/*.js'
   ],
 
-  seleniumArgs: ['-browserTimeout=60'],
+  //seleniumArgs: ['-browserTimeout=60'],
   seleniumAddress: 'http://0.0.0.0:4444/wd/hub',
 
-  capabitilies: {
+  // capabitilies: {
+  //   'browserName': 'firefox'
+  // },
+
+  multiCapabilities: [{
+
+    'browserName': 'firefox'
+  }, {
     'browserName': 'chrome'
-  },
+  }],
 
-  framework: 'jasmine',
 
-  baseUrl: 'http://127.0.0.1:8000',
 
-  allScriptsTimeout: 30000,
+  baseUrl: 'http://127.0.0.1:8000'
 
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
+
+  // framework: 'jasmine',
+
+  // onPrepare: function() {
+  //   browser.driver.manage().window().maximize();
+  // },
+
+  // // Options to be passed to Jasmine-node.
+  // jasmineNodeOpts: {
+  //   showColors: true,
+  //   defaultTimeoutInterval: 30000
+  // }
 
 };
